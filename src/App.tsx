7307817276b09c23d703fc27f38d4f75c0858e6e
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-28 00:09:35 +0900

    Material UI install / Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import BarPlot from "./components/BarPlot";
 import BubblePlot from "./components/BubblePlot";
 import LinePlot from "./components/LinePlot";
+import MaterialUI from "./components/MaterialUI";
 import PiePlot from "./components/PiePlot";
 import RadarPlot from "./components/RadarPlot";
 
 function App() {
   return (
     <div className="App">
+      <MaterialUI />
       <LinePlot />
       <PiePlot />
       <BarPlot />
---
+++ src/components/MaterialUI.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@material-ui/core";
+import React, { FC } from "react";
+
+const MaterialUI: FC = () => {
+  return (
+    <div>
+      <Button variant="contained" color="secondary">
+        Test Button
+      </Button>
+    </div>
+  );
+};
+
+export default MaterialUI;
